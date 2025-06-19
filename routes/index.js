162e6ecabf0ec0f785dad53700efc3ce0d120a35
emmanuel2011 <okoyee61@gmail.com>
2025-06-19 01:59:19 +0100

    edited

--- routes/index.js
+++ routes/index.js
@@ -1,19 +1,19 @@
-const express = require('express')
-const router = express.Router();
+const express = require('express');
 const passport = require('passport');
+const router = express.Router();
 
-
+router.use('/', require('./swagger'));
 router.use('/user', require('./user'));
 router.use('/theme', require('./theme'));
 
-router.get('/login', passport.authenticate('github'),(req,res)=> {});
+router.get('/login', passport.authenticate('github'));
+
 router.get('/logout', (req, res, next) => {
-  req.logout((err) => {
-    if (err) {
-      return next(err); 
-    }
+  req.logout(err => {
+    if (err) return next(err);
     res.redirect('/');
   });
 });
+
 
 module.exports = router;
